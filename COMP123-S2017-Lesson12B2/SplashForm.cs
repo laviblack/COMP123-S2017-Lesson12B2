c457e0e03911a01571880bad0034498388810fe6
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-08-09 03:30:48 -0400

    Added SplashForm

---
+++ COMP123-S2017-Lesson12B2/SplashForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ Name: Lyrica Yoshida
+ ID: 300923951
+ Date: August 8, 2017
+ Description: This is the SplashForm class
+ Version: 0.1 - Created the SplashForm
+     */
+
+namespace COMP123_S2017_Lesson12B2
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
