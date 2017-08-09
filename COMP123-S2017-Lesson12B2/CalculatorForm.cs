f3d60c09ae085f24e3d558167e9431b6d33807b2
Lyrica Yoshida <lyoshida@my.centennialcollege.ca>
2017-08-09 03:54:34 -0400

    Added an image with picture box

--- COMP123-S2017-Lesson12B2/CalculatorForm.cs
+++ COMP123-S2017-Lesson12B2/CalculatorForm.cs
@@ -32,7 +32,7 @@ namespace COMP123_S2017_Lesson12B2
         /// <param name="e"></param>
         private void CalculatorForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
